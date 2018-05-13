39404f2c7e35030a6a22ea976809d4e7ea076f46
belajarcodegit <khairus.hendriansyah@gmail.com>
2018-05-13 14:20:35 +0700

    dirubah dikit

--- test php.php
+++ test php.php
@@ -1,10 +1,13 @@
-     <?php
-        // put your code here
-        echo 'aku adalah uji coba git';  
-        echo 'aku adalah uji coba git';  
-        echo 'aku adalah uji coba git';  
-        echo 'aku adalah uji coba git';  
-        echo 'aku adalah uji coba git';  
-     
-     ?>
+<?php
+
+// put your code here
+echo 'aku adalah uji coba git';
+echo 'aku adalah uji coba git';
+echo 'aku adalah uji coba git';
+echo 'aku adalah uji coba git';
+echo 'aku adalah uji coba git';
+
+$param = DateTime::RFC822;
+echo $param;
+?>
 
